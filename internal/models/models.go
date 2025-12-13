f1b5e35dfb98ac4de751d64331a2e9257877001c
Moncef Ahmane <moncefahmane01@gmail.com>
2025-12-13 09:47:36 +0100

    Added Agenda and Alert Models

--- internal/models/models.go
+++ internal/models/models.go
@@ -8,3 +8,13 @@ type User struct {
 	Id   *uuid.UUID `json:"id"`
 	Name string     `json:"name"`
 }
+type Agenda struct {
+	ID    uuid.UUID // identifiant interne
+	Name  string    // nom lisible (ex: "M1 ISIMA groupe A")
+	UcaID string    // identifiant UCA de l’EDT
+}
+type Alert struct {
+	ID       uuid.UUID // identifiant interne
+	AgendaID uuid.UUID // lien vers l’agenda surveillé
+	Email    string    // destinataire
+}
